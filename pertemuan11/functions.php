56fecb625fa358de111c934b35aa040b724ebfe2
rickosong <ricko.aprilianto00@gmail.com>
2022-01-21 22:36:55 +0800

    make folder pertemuan11

---
+++ pertemuan11/functions.php
@@ -0,0 +1,50 @@
+<?php 
+
+// koneksi ke database (hostname, username, password, database name)
+$conn_db = mysqli_connect("localhost", "root", "", "mahasiswa_db");
+
+// catatan functions
+// function query = nama function
+// ($query) merupakan sebuah permasalahan yg dipakakan ke fungsi query. contoh dalam kasus ini adalah "(SELECT * fORM mahasiswa) 
+function query($query) {
+    global $conn_db;
+    $result = mysqli_query($conn_db, $query);
+    $rows = [];
+    while ( $row = mysqli_fetch_assoc($result) ) {
+        $rows[] = $row;
+    }
+
+    return $rows;
+}
+
+function tambah($data){
+    global $conn_db;
+
+        // ambil data dari elemen dalam form
+        $nrp = htmlspecialchars($data["nrp"]);
+        $nama = htmlspecialchars($data["nama"]);
+        $email = htmlspecialchars($data["email"]);
+        $jurusan = htmlspecialchars($data["jurusan"]);
+        $gambar = htmlspecialchars($data["gambar"]);
+
+        // query insert data
+        $query = "INSERT INTO mahasiswa
+        values('','$nrp', '$nama', '$email', '$jurusan', '$gambar')
+        "; 
+
+        mysqli_query($conn_db, $query);
+
+    return mysqli_affected_rows($conn_db);
+}
+
+
+// logika hapus data adalah ambil dulu id baru dimasukkan ke $id, setelah itu masukkan $id ke dalam parameter fungsi hapus, di fungsi hapus kita delete id yg terpilih menggunakan sql, lalu tampilkan affecterd rowsnya.
+function hapus($data){
+    global $conn_db;
+
+    mysqli_query($conn_db, "DELETE FROM mahasiswa WHERE id = $data ");
+
+    return mysqli_affected_rows($conn_db);
+}
+
+?>
---
+++ pertemuan11/index.php
@@ -0,0 +1,58 @@
+<?php
+
+require 'functions.php';
+
+$mahasiswa = query( "SELECT * FROM mahasiswa" );
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Admin</title>
+    <style>
+        img {
+            width: 80px;
+            height: 80px;
+        }
+    </style>
+</head>
+<body>
+    <h1>Daftar Mahasiswa</h1>
+    <a href="tambahdata.php">Tambah Data Mahasiswa</a>
+    <br><br>
+
+    <table border="1" cellspacing="0" cellpadding="10">
+        <tr>
+            <th>No.</th>
+            <th>Aksi</th>
+            <th>Gambar</th>
+            <th>NRP</th>
+            <th>Nama</th>
+            <th>Email</th>
+            <th>Jurusan</th>
+        </tr>
+
+        <?php $i=1 ?>
+        <?php foreach ( $mahasiswa as $row  ) :  ?>
+        <tr>
+            <td><?= $i ?></td>
+            <td>
+                <a href="">ubah | </a>
+                <a href="hapus.php?id=<?= $row["id"] ?>" onclick="return confirm('Apakah Anda Yakin Ingin Menghapus data ini')">hapus</a>
+            </td>
+            <td><img src="img/<?= $row["gambar"] ?>" alt=""></td>
+            <td><?= $row["nrp"] ?></td>
+            <td><?= $row["nama"] ?></td>
+            <td><?= $row["email"] ?></td>
+            <td><?= $row["jurusan"] ?></td>
+        </tr>
+        <?php $i++ ?>
+        <?php endforeach  ?>
+    </table>
+</body>
+</html>
