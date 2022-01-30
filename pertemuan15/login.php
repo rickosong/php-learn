ca0eeec718b4ff73c184ffade3760031bd720570
rickosong <ricko.aprilianto00@gmail.com>
2022-01-31 00:57:05 +0800

    make file login.php

---
+++ pertemuan15/login.php
@@ -0,0 +1,22 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <h1>Form Login</h1>
+
+    <form action="" method="post">
+    <label for="username">Username:</label><br>
+        <input type="text" name="username" id="username"><br><br>
+        <label for="password">Password:</label><br>
+        <input type="password" name="password" id="password"><br><br>
+        <button type="submit" name="login">Login</button>
+    </form>
+</body>
+</html>
