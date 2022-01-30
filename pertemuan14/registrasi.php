b8f45883db13b751761793ecf935112d7b84cbf0
rickosong <ricko.aprilianto00@gmail.com>
2022-01-30 23:15:21 +0800

    membuat form registrasi

---
+++ pertemuan14/registrasi.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registrasi</title>
+</head>
+<body>
+    <h1>Halaman Registrasi</h1>
+
+    <form action="" method="post">
+        <label for="username">Username</label><br>
+        <input type="text" name="username" id="username"><br><br>
+        <label for="password">Password</label><br>
+        <input type="password" name="password" id="password"><br><br>
+        <label for="password">Konfirmasi Password</label><br>
+        <input type="password" name="password" id="password"><br><br>
+        <button type="submit" name="register">Register</button>
+    </form>
+</body>
+</html>
