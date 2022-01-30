e232804d33a1677e414962856e4667720d330f77
rickosong <ricko.aprilianto00@gmail.com>
2022-01-30 23:33:04 +0800

    membuat syntax php di registrasi.php

--- pertemuan14/registrasi.php
+++ pertemuan14/registrasi.php
@@ -1,3 +1,20 @@
+<?php 
+require "functions.php";
+
+
+if (isset($_POST["register"])) {
+
+    if (registrasi($_POST) > 0) {
+        Echo "<script> alert('Registrasi Berhasil!')  </script>";
+    }
+    else {
+        echo mysqli_error($conn_db);
+    }
+
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,11 +27,11 @@
     <h1>Halaman Registrasi</h1>
 
     <form action="" method="post">
-        <label for="username">Username</label><br>
+        <label for="username">Username:</label><br>
         <input type="text" name="username" id="username"><br><br>
-        <label for="password">Password</label><br>
+        <label for="password">Password:</label><br>
         <input type="password" name="password" id="password"><br><br>
-        <label for="password">Konfirmasi Password</label><br>
+        <label for="password">Konfirmasi Password:</label><br>
         <input type="password" name="password" id="password"><br><br>
         <button type="submit" name="register">Register</button>
     </form>
